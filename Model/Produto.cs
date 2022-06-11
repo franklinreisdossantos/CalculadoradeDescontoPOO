1c183e04c98ed2135068440518538a2946447f30
franklinreis18 <kinho-rds@hotmail.com>
2022-06-11 20:07:41 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade2Fiap1106.Model
+{
+     class Produto
+    {
+        //Propiedades informaçoes que o objeto armazena
+        public string Idproduto { get; set; }
+        public  string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Pneu { get; set; }
+
+        public int Estoque { get; set; }
+
+        public string Marca { get; set; }
+
+        //acões queobjeto pode executar
+        public void Exibirdados()
+        {
+            Console.WriteLine($"***Id do produto:{Idproduto}***\n***  Nome:{Nome}***\n*** Preço: {Preco}***\n *** Estoque: {Estoque}***\n ***Marca:{Marca}***");
+        }
+
+        public decimal CalculadoraValordedesconto( decimal  Valordedesconto)
+        {
+            decimal valorfinal = Preco - Valordedesconto;
+            return valorfinal;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Atividade2Fiap1106.Model;
+
+
+
+namespace Atividade2Fiap1106
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ler dados do imput do usuário
+            Console.WriteLine("Digite o Id");
+            var id = (Console.ReadLine());
+
+            Console.WriteLine("Nome do produto");
+            string nome =(Console.ReadLine());
+
+            Console.WriteLine("Digite o Preço");
+            decimal preco = decimal.Parse(Console.ReadLine());
+
+           Console.WriteLine("Digite o estoque");
+            int stoque =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite amarca");
+            string marca = Console.ReadLine();
+            Console.Clear();
+
+             //Criando o objeto
+            Produto produto = new Produto();
+            produto.Idproduto = id;
+            produto.Nome = nome;
+            produto.Preco = preco;
+            produto.Estoque = stoque;
+            produto.Marca = marca;
+
+            //Chamar o metodo
+            produto.Exibirdados();
+
+            //Chamando o metodo com desconto
+            Console.WriteLine("Digite o desconto");
+            decimal Valordedesconto1 = decimal.Parse(Console.ReadLine());
+            decimal valor = produto.CalculadoraValordedesconto(Valordedesconto1);
+            Console.WriteLine($"Valor do desconto {valor}");
+            Console.ReadKey();
+     
+
+
+            
+
+
+
+
+        }
+    }
+}
